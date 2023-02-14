2b27098e947f9a0af3497029eac72dfaa3534ed1
Urooj Ahmad <uroojahamad786@gmail.com>
2023-02-14 23:52:12 +0530

    adding tenzie game component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// import logo from './logo.svg';
+// import './App.css';
+import TenziesGame from './components/TenziesGame';
+
+function App() {
+  return (
+    <TenziesGame />
+  );
+}
+
+export default App;
---
+++ src/components/TenziesGame.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import TenziesGameBoard from "./TenziesGameBoard";
+import TenziesGameHeader from "./TenziesGameHeader";
+
+const TenziesGame = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className='flex flex-col border border-red-500 min-h-screen sm:w-3/5 p-7'>
+        <TenziesGameHeader />
+        <TenziesGameBoard />
+      </div>
+    </div>
+  );
+};
+
+export default TenziesGame;
