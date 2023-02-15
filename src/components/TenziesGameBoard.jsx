e784e7788281f1278215d2f625c05ba4be4380d4
Urooj Ahmad <uroojahamad786@gmail.com>
2023-02-15 23:19:06 +0530

    adding tenzie game board component

---
+++ src/components/TenziesGameBoard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import RollButton from './RollButton'
+
+const TenziesGameBoard = () => {
+  return (
+    <>
+        <div className='border border-black p-10 h-96 sm:h-80'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, qui voluptatem odio dignissimos quia ipsum. Dolorum accusamus necessitatibus debitis iusto excepturi, magni vel commodi quisquam sequi dolore? Soluta porro ipsum quia beatae, ex eaque. Velit esse beatae delectus quidem ratione!
+        </div>
+        <RollButton />
+    </>
+  )
+}
+
+export default TenziesGameBoard
