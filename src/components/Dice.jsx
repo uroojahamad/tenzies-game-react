0c08d66ec27ebe183012ef0a4cae55c5895234c5
Urooj Ahmad <uroojahamad786@gmail.com>
2023-02-16 20:05:05 +0530

    removed border and increased font size

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Dice = () => {
   return (
     <div className='w-12 h-12 flex justify-center items-center cursor-pointer rounded-xl bg-white shadow-2xl'>
-      <span className='text-center font-mono font-light'>1</span>
+      <span className='text-center font-mono font-bold text-2xl'>1</span>
     </div>
 
   )
--- src/components/TenziesGameHeader.jsx
+++ src/components/TenziesGameHeader.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const TenziesGameHeader = () => {
   return (
-    <div className="flex flex-col justify-center items-center border border-black bg-slate-100 p-10 sm:h-48 lg:h-30">
-      <h1 className="text-2xl font-mono font-extrabold p-3 sm:mt-3 sm:p-2">
+    <div className="flex flex-col space-y-3 justify-center items-center border border-slate-300 bg-slate-100 p-10 sm:h-48 lg:h-30">
+      <h1 className="text-2xl font-mono font-extrabold p-3 sm:mt-3 sm:p-2 md:text-4xl">
         Tenzies Game
       </h1>
       <p className="text-xl font-mono font-medium sm:pb-7 sm:text-lg">
