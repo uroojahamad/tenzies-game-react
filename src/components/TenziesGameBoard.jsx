194e867e2356422728d5198444033d9e11bbfee1
Urooj Ahmad <uroojahamad786@gmail.com>
2023-02-17 15:14:57 +0530

    adding Tenzie game board component

--- src/components/TenziesGameBoard.jsx
+++ src/components/TenziesGameBoard.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
-import RollButton from './RollButton'
+import React from "react";
+import Dice from "./Dice";
 
 const TenziesGameBoard = () => {
   return (
     <>
-        <div className='border border-black p-10 h-96 sm:h-80'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, qui voluptatem odio dignissimos quia ipsum. Dolorum accusamus necessitatibus debitis iusto excepturi, magni vel commodi quisquam sequi dolore? Soluta porro ipsum quia beatae, ex eaque. Velit esse beatae delectus quidem ratione!
+      <div className="border-b border-slate-300 bg-slate-100 p-10 h-96 sm:h-60 flex justify-center items-center">
+        <div className="grid grid-cols-4 sm:grid-cols-4 md:grid-cols-5 gap-5 lg:gap-10 p-3 shadow-2xl rounded-xl pt-4 bg-slate-100">
+          <Dice />
+          <Dice />
+          <Dice />
+          <Dice />
+          <Dice />
+          <Dice />
+          <Dice />
+          <Dice />
+          <Dice />
+          <Dice />
         </div>
-        <RollButton />
-    </>
-  )
-}
+      </div>
 
-export default TenziesGameBoard
+      {/* Roll button */}
+      <div className="flex justify-center items-center bg-slate-100 p-10 h-32 sm:h-20">
+        <button className="w-60 h-14 sm:w-40 sm:h-10 lg:w-60 lg:h-14 border border-blue-500 bg-blue-500 cursor-pointer text-white rounded-3xl shadow-md shadow-blue-200 hover:shadow-lg hover:shadow-blue-600 text-3xl font-mono">
+          Roll
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default TenziesGameBoard;
