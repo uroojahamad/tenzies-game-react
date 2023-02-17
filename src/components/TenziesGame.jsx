43c6ece0af79acaa98728a31b0191a16cb58c660
Urooj Ahmad <uroojahamad786@gmail.com>
2023-02-17 15:14:24 +0530

    adding Tenzie game component

--- src/components/TenziesGame.jsx
+++ src/components/TenziesGame.jsx
@@ -5,7 +5,7 @@ import TenziesGameHeader from "./TenziesGameHeader";
 const TenziesGame = () => {
   return (
     <div className="flex justify-center items-center">
-      <div className='flex flex-col border border-red-500 min-h-screen sm:w-3/5 p-7'>
+      <div className='flex flex-col bg-zinc-800 min-h-screen sm:w-3/5 p-7'>
         <TenziesGameHeader />
         <TenziesGameBoard />
       </div>
