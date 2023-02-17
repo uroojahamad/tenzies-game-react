5d076a6b4df0b4db1e15efa9ec4ca122f3a2708f
Urooj Ahmad <uroojahamad786@gmail.com>
2023-02-18 00:55:20 +0530

    styled the dice component

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Dice = () => {
+const Dice = (props) => {
+
   return (
-    <div className='w-12 h-12 flex justify-center items-center cursor-pointer rounded-xl bg-white shadow-2xl'>
-      <span className='text-center font-mono font-bold text-2xl'>1</span>
+    <div className='w-12 h-12 flex justify-center items-center cursor-pointer rounded-xl bg-white shadow-2xl hover:-translate-y-1 border border-slate-200 hover:bg-green-300'>
+      <span className='text-center font-mono font-bold text-2xl'>{props.value}</span>
     </div>
 
   )
