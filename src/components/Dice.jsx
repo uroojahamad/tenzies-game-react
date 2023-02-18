d71d49c3ad39f89c4f3c7a2cb0e7741e895dd9f9
Urooj Ahmad <uroojahamad786@gmail.com>
2023-02-18 17:22:12 +0530

    added condition to set bg color

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Dice = (props) => {
 
+  const {value, isHeld, holdDice} = props
+  
   return (
-    <div className='w-12 h-12 flex justify-center items-center cursor-pointer rounded-xl bg-white shadow-2xl hover:-translate-y-1 border border-slate-200 hover:bg-green-300'>
-      <span className='text-center font-mono font-bold text-2xl'>{props.value}</span>
+    <div className={`w-12 h-12 flex justify-center items-center cursor-pointer rounded-xl shadow-2xl hover:-translate-y-1 border border-slate-200 hover:bg-green-300 ${isHeld ? 'bg-green-400' : 'bg-white'}`} onClick={holdDice}>
+      <span className='text-center font-mono font-bold text-2xl'>{value}</span>
     </div>
 
   )
